c2eb55a82a4b8e01e9db0770b4590bc0359bd97b
M. Arhum Qayyum Khan <khanarhumqayyum@gmail.com>
2025-05-05 01:48:05 +0500

    Update CV-screening.py

--- beckend/CV-screening/CV-screening.py
+++ beckend/CV-screening/CV-screening.py
@@ -3,4 +3,13 @@ import streamlit as st
 def main():
     st.set_page_config(page_title="CV-Screening", page_icon=":books:")
 
-    st.header("CV-Screening")
+    st.header("CV-Screening :books:")
+    st.text_input("Ask a question about your documents:")
+
+    with st.sidebar:
+        st.subheader("Your documents")
+        st.file_uploader("Upload your PDFs here and click on 'Process'")
+        st.button("Process")
+
+if __name__ == '__main__':
+    main()
