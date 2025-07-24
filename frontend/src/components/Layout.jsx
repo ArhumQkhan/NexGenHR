4326b0a0371a0805fdb420cf2d07c402d6f68bbb
beesmasharif <beesma.sharif99@gmail.com>
2025-07-25 03:08:33 +0500

    employee login UI

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,19 +1,28 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 import Footer from "./Footer";
 
 function Layout({ children }) {
+  const location = useLocation();
+
+  // Define routes where Footer should be hidden
+  const hideFooterRoutes = [];
+
+  // Determine whether to show the footer
+  const shouldShowFooter = !hideFooterRoutes.includes(location.pathname);
+
   return (
     <div className="layout">
       <div className="scroll-bar"></div>
 
-      {/* Single Background Image */}
+      {/* Background */}
       <div className="background-container"></div>
 
       {/* Page Content */}
       <div className="content">{children}</div>
 
-      {/* Footer */}
-      <Footer />
+      {/* Footer (conditionally shown) */}
+      {shouldShowFooter && <Footer />}
     </div>
   );
 }
