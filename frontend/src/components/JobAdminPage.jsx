dd3f7ffee73518d5799c46d8a946c181bd17dfe2
beesmasharif <beesma.sharif99@gmail.com>
2025-07-28 17:39:00 +0500

    admin job posting and admin dashboard(cleared alignment issue)

--- frontend/src/components/JobAdminPage.jsx
+++ frontend/src/components/JobAdminPage.jsx
@@ -1,34 +1,20 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import Topbar from "./Topbar";
 import CreateJobPost from "../pages/CreateJobPost";
 import AdminJobPostList from "../pages/AdminJobPostList";
 
 const JobAdminPage = () => {
-  const navigate = useNavigate();
-
-  const handleGoHome = () => {
-    navigate("/Employee"); // update path if needed
-  };
-
   return (
-    <div className="container mt-4">
-      <div className="row align-items-center mb-2">
-        <div className="col-4 text-start ">
-          <button className="btn btn-primary"
-            style={{ width: "100px" }}   onClick={handleGoHome}>
-            ← Home
-          </button>
+    <>
+      <Topbar />
+      <div className="container mt-4">
+        <div className="row align-items-center mb-2">
         </div>
-        <div className="col-4 text-center">
-          <h2 className="text-primary m-0">Admin Job Post Panel</h2>
-        </div>
-        <div className="col-4">{/* Right side empty for spacing */}</div>
+        <CreateJobPost />
+        <hr className="my-4" />
+        <AdminJobPostList />
       </div>
-
-      <CreateJobPost />
-      <hr className="my-4" />
-      <AdminJobPostList />
-    </div>
+    </>
   );
 };
 
